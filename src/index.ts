fd3beeb68dae0c5a7691fde74cb5fa0afe5b61dc
BenjaminFlan <Benjamin.Flanagan@kainos.com>
2026-07-07 14:16:07 +0100

    :)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
-import { app } from './app';
 
 dotenv.config();
 
-const PORT = Number(process.env.PORT ?? '3000');
+import { app } from './app';
+
+const PORT = Number(process.env.PORT ?? '3001');
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
