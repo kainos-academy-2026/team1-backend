774a4a58bf7fd139ed9c17f7e392e37a7d703e5e
BenjaminFlan <Benjamin.Flanagan@kainos.com>
2026-07-10 10:07:00 +0100

    linting

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ if (!connectionString) {
 }
 
 const PORT = Number(process.env.PORT) || 3001;
-	
+
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
 	console.log(`Try: http://localhost:${PORT}/health`);
