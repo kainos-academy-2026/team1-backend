0911555dfb45e75f1348f660ae093fc5f4dd8be4
WoodyHenderson <woodyhenderson@hotmail.com>
2026-07-30 12:11:35 +0100

    commit

---
+++ my-infrastructure/azurem_key_vault.tf
@@ -0,0 +1,15 @@
+resource "azurerm_key_vault" "app" {
+  name                = "kv-team1-backend-dev"
+  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  purge_protection_enabled   = true
+  soft_delete_retention_days = 7
+
+  rbac_authorization_enabled    = true
+  public_network_access_enabled = true
+}
+
+data "azurerm_client_config" "current" {}
